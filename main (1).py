70b49753c4213e4e21b27875ffd4bf2475cd85b6
Perrofa <111369005+Perrofa@users.noreply.github.com>
2022-08-26 23:05:24 -0500

    A01412171 Alan Herrera Martínez
    Contexto:
    A la hora de pensar sobre que podria hacer como parte de mi trabajo a futuro decidi intentar hacer un juego, escogi
    un juego de preguntas el cual a mi punto de vista podria ser un reto para mi persona, es algo sencillo para empezar pero 
    en este momento es lo mejor. El juego que escogi fue por que quise empezar a crear mas juegos para poder hagarrar
    practica mas en este ambito. las skins del juego al igual que el sonido si me dejan usare el del juego, si no usare unas
    que esten disponibles por que honestamente hacer esto mismo me quedaria mal y seria muy pesado. Lo escogi por me parece 
    interesante el poder crecer en algo en lo que carezco. 
    
    Pseudocodigo:
    
    Eo (respuesta)
    
      Escribir "¿El presidente de Mexico es Andres Manuel Lopez Obrador?" 
      mostrar respuesta
      si respuesta es igual a "si" entonces 
        Escribir "¿Benito Juarez era muy alto?"
        mostrar respuesta 
        si respuesta es igual a "si" entonces
         Escribir "¿Es un pollo el logo de la bandera de Mexico?
         mostrar respuesta
         si respuesta es igual a "si" entonces 
          Escribir "¿El dia son 24 horas?"
          si respuesta es igual a "si" entonces
           Escribir "Ganaste"
          si no 
           Escribir "Perdiste"
         si no
          Escribir "Perdiste"
        si no
         Escribir "Perdiste"
      si no
       si no
    Ef

---
+++ main (1).py
@@ -0,0 +1,13 @@
+#A01412171 Alan Herrera Martínez 
+puntaje = 0
+respuesta = ""
+
+print("¿El presidente de Mexico es Andres Manuel Lopez Obrador?")
+respuesta = input()
+if respuesta == "si":
+  puntaje = puntaje + 1
+else:
+  puntaje = puntaje - 1
+
+print (respuesta)
+print (puntaje)
