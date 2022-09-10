4f4c1f227204620be7de7d86859415d0bd7c73df
Perrofa <alanmartinex@hotmail.com>
2022-09-09 20:31:11 -0500

    Nueva pregunta/diseño

--- main (1).py
+++ main (1).py
@@ -1,5 +1,4 @@
 #A01412171 Alan Herrera Martínez 
-
 puntaje = 0
 respuesta = ""
 jugadores = 0
@@ -9,18 +8,25 @@ jugadores = int(input())
 
 while jugadores != 0:
   puntaje = 0
-  print("Jugador " + str(jugadores) + ": El presidente de Mexico es Andres Manuel Lopez Obrador?")
+  
+  print("Jugador " + str(jugadores) + ": El presidente de Mexico es Andres Manuel Lopez Obrador? (si/no)")
   respuesta = input()
   if respuesta == "si":
    puntaje = puntaje + 1
   else:
    puntaje = puntaje - 1
-
+  
+  print("Jugador " + str(jugadores) + ": Benito Juarez era muy alto?(si/no)")
+  respuesta = input()
+  if respuesta == "no":
+   puntaje = puntaje + 1
+  else:
+   puntaje = puntaje - 1
+  
   print("Jugador " + str(jugadores) + " Puntuaje es igual a " + str(puntaje))
+  print("======================================================================================")
   jugadores = jugadores - 1
   
-
-
 
 
 
